18b02d55963375739cf9cb89a2facfdccc0002ca
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2020-11-08 21:03:45 +0530

    to color the blocked word

--- script.js
+++ script.js
@@ -7,12 +7,20 @@ replaceText(document.body)
      }
     
 else if (element.nodeType === Text.TEXT_NODE){
-     //to paragraph
+    
      if(element.textContent.match(/sex|porn/gi)){
-        element.parentElement.style.color ='black'
-        element.parentElement.style.backgroundColor = 'black'
+        //color the blocked word
+        const newElement = document.createElement('span')
+        newElement.innerHTML = element.textContent.replace(/(sex|porn)/gi,
+        '<span style="background-color: black; color:black">$1</span>'
+        )
+        element.replaceWith(newElement)
+
+          //to paragraph
+    //     element.parentElement.style.color ='black'
+    //     element.parentElement.style.backgroundColor = 'black'
      }
      //to change word to no
-        // element.textContent = element.textContent.replace(/sex|porn/gi,'No')
+         element.textContent = element.textContent.replace(/sex|porn/gi,'No')
      }
- }
+ } 
