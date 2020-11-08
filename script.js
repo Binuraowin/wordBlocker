53c9e6d611258aef737b9539e14b18d654533f56
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2020-11-08 20:11:29 +0530

    to hightlight a paragraph contains blocked word

--- script.js
+++ script.js
@@ -4,7 +4,15 @@ replaceText(document.body)
  function replaceText(element){
      if( element.hasChildNodes()){
          element.childNodes.forEach(replaceText)
-     }else if (element.nodeType === Text.TEXT_NODE){
-         element.textContent = element.textContent.replace(/coronavirus|sex|porn/gi,'No')
+     }
+    
+else if (element.nodeType === Text.TEXT_NODE){
+     //to paragraph
+     if(element.textContent.match(/sex|porn/gi)){
+        element.parentElement.style.color ='black'
+        element.parentElement.style.backgroundColor = 'black'
+     }
+     //to change word to no
+        // element.textContent = element.textContent.replace(/sex|porn/gi,'No')
      }
  }
