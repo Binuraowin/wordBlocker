77f1bbe3e25de89d45c3b76821a5e444962fa99d
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2020-11-09 00:25:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,16 +19,16 @@ else if (element.nodeType === Text.TEXT_NODE){
         //to remove the blocked word
        element.parentElement.remove()
 
-      //to animate word
-    //    const newElement = document.createElement('span')
-    //     newElement.innerHTML = element.textContent.replace(/(sex|porn)/gi,
-    //     '<span class="rainbow">$1</span>'
-    //     )
-    //     element.replaceWith(newElement)
+       //to animate word
+       //    const newElement = document.createElement('span')
+       //     newElement.innerHTML = element.textContent.replace(/(sex|porn)/gi,
+       //     '<span class="rainbow">$1</span>'
+       //     )
+       //     element.replaceWith(newElement)
 
-          //to paragraph
-    //     element.parentElement.style.color ='black'
-    //     element.parentElement.style.backgroundColor = 'black'
+        //to paragraph
+        //     element.parentElement.style.color ='black'
+        //     element.parentElement.style.backgroundColor = 'black'
      }
      //to change word to no
          element.textContent = element.textContent.replace(/sex|porn/gi,'No')
